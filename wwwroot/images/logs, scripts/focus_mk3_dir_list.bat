7d2330f308ddd82608f16b1ea9b51354f107142d
Gábor Marschall <56896497+CaTwoPlus@users.noreply.github.com>
2023-09-05 19:08:52 +0200

    Delete wwwroot/images/logs, scripts directory

--- wwwroot/images/logs, scripts/focus_mk3_dir_list.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set dirpath=%1
-dir %dirpath% /-p /o:gn > "%dirpath%\DirContents.txt"
-exit
--- wwwroot/images/logs, scripts/rename_files_by_delete.ps1
+++
@@ -1,8 +0,0 @@
-$folderPath = Get-Location
-
-Get-ChildItem -Path $folderPath -Recurse -File | ForEach-Object {
-    $newName = $_.Name -replace "FOCUS ", ""
-    if($newName -ne $_.Name){
-        Rename-Item -Path $_.FullName -NewName $newName -WhatIf
-    }
-}
