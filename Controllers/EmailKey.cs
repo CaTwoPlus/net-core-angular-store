bc95fd3823020ff9469d23fdb3e5c263782d1974
Gábor Marschall <razor0109@gmail.com>
2023-09-14 21:36:35 +0200

    #41

---
+++ Controllers/EmailKey.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace BontoAPI.Controllers
+{
+    [Route("api/validate")]
+    [ApiController]
+    public class EmailKey : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetValidationKey()
+        {
+            var apiKey = System.Environment.GetEnvironmentVariable("reCAPTCHA_CLIENT_API_KEY");
+            if (apiKey == null)
+            {
+                return BadRequest("Could not retrieve reCAPTCHA API key!");
+            }
+            return Ok(apiKey);
+        }
+    }
+}
--- angular/src/environments/environment.prod.ts
+++ angular/src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const prodEnvironment = {
     production: true,
     baseUrl: 'https://ford-bonto.azurewebsites.net', 
     apiUrl: 'https://ford-bonto.azurewebsites.net/api', 
+    reCAPTCHASiteKey: 'reCAPTCHA_CLIENT_API_KEY'
   };
   
